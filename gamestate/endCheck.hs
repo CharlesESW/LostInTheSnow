1c5b17e5bed29e1d74d5accde79af7f4bc175c89
exg046 <erose1627@gmail.com>
2024-11-02 22:08:21 +0000

    isEndRoom check done

--- gamestate/endCheck.hs
+++ gamestate/endCheck.hs
@@ -3,31 +3,41 @@ module EndCheck where
 import Gamestate.GameState
 import Control.Monad.State
 
+{-
+
+Immediate Death ends the game
+3 Flags ends the game
+Reaching the end ends the game
+
+Successful end means not triggering moralGameOver
+
+--need add flag function when a flag is triggered
+EXTRA--could remove flags if find and use healing supplies
+-}
+
 containsValue :: (Eq v) => v -> [v] -> Bool
 containsValue _ [] = False
 containsValue x (y:ys)
     | x == y = True
     | otherwise = containsValue x ys
 
-    {-
-isEndRoom :: State GameState Bool
-isEndRoom = do
-    gameState <- get
-    let 
--}
---game over if reach end and moralFlag has gone off
-
-
-moralGameOver :: State GameState Bool
-moralGameOver = do
+--if bunny is killed, then reaching the end is still a failure
+failedEndBool :: State GameState Bool
+failedEndBool = do
     game <- get
     let flagDeath = flags game
     return $ containsValue "bunny" flagDeath
 
-
---game over if flags set off
+--if 3 flags are triggered then failed and end early
 flagGameOver :: State GameState Bool
 flagGameOver = do
     gameState <- get
     let flagDeath = flags gameState
     if length flagDeath >=3 then return True else return False
+
+--find out if end room has been reached
+isEndRoom :: State GameState Bool
+isEndRoom = do
+    gameState <- get
+    let lastScene = scene gameState
+    if lastScene == 16 then return True else return False
